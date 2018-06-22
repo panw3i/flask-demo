b0e04e36c583fc2ba1cf73ff0c261e113735421a
panw3i <panw3i@gmail.com>
2018-06-22 23:58:55 +0800

    修改代码不用重启服务

--- start.py
+++ start.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 # 路由最小原则
 @app.route('/hello')
 def hello():
-    return 'hello flask!'
+    return 'hello flask!!!!'
 
-app.run()
+
+app.run(debug=True)

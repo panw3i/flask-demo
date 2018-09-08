3d62af1504ff24259d00f758f269e4fa3ec1693d
panw3i <panw3i@gmail.com>
2018-09-08 22:42:12 +0800

    添加配置文件

--- start.py
+++ start.py
@@ -1,13 +1,21 @@
+# coding:utf-8
+
 from flask import Flask
+
+# from config import *
 
 app = Flask(__name__)
 
+# 载入配置文件
+app.config.from_object('config')
 
-# 路由最小原则
-# @app.route('/hello')
+
+@app.route("/hello")
 def hello():
-    return 'hello flask!!!!'
+    return "hello world"
 
-app.add_url_rule('/hello',view_func=hello)
 
-app.run(host='0.0.0.0',debug=True)
+# 在生产环境是不会执行的,只是做为uwsgi的一个模块
+if __name__ == '__main__':
+    # 外网可访问地址
+    app.run(host="0.0.0.0", debug=app.config['DEBUG'], port=app.config['PORT'])
